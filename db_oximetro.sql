0be716063cbcf870a93c120d2cd1d35cc5c8d1dd
Daniel Ramírez <64878115+Adro312@users.noreply.github.com>
2022-10-28 13:27:20 -0500

    db configurada

--- db_oximetro.sql
+++ db_oximetro.sql
@@ -9,12 +9,15 @@ CREATE TABLE records (
   registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO records VALUES (null, 94, 80, 36.5, null), (null, 92, 60, 36.3, null), (null, 90, 89, 36.9, null);
+
 CREATE TABLE oximetro_status (
   id INT PRIMARY KEY AUTO_INCREMENT,
   oximetro_connection INT DEFAULT 0,
   scan_status INT DEFAULT 0
 );
 
--- Prubeas
 INSERT INTO oximetro_status (oximetro_connection, scan_status) VALUES (0, 0);
 UPDATE oximetro_status SET scan_status = 1 WHERE id = 1;
+
+SELECT id, oxygen, heart_rate, temperature, DATE(registration_date) AS 'Date', TIME(registration_date) AS 'Hour' FROM records;
